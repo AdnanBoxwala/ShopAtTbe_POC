42ea459620c42e576719870d9c2f18f19ad3a1b2
AdnanBoxwala <143093290+AdnanBoxwala@users.noreply.github.com>
2024-05-06 22:45:55 +0200

    part of previous commit

--- ShopAtTBE/Views/Root/CustomerView.swift
+++ ShopAtTBE/Views/Root/CustomerView.swift
@@ -9,33 +9,14 @@ import SwiftUI
 
 struct CustomerView: View {
     @State var viewModel = ViewModel()
-    @State private var selectedTab = "Catalog"
-    
-    let user: User
-    let isAnonymous: Bool
-    
+
     var body: some View {
-        TabView(selection: $selectedTab) {
-            CatalogView()
-                .tabItem { Label("T.B.E", systemImage: "house") }
-                .tag("Catalog")
-            BagView()
-                .tabItem { Label("Bag", systemImage: "handbag.fill") }
-                .tag("Bag")
-//            ProfileView(user: user, isAnonymous: isAnonymous)
-//                .tabItem { Label("Profile", systemImage: "person.circle.fill") }
-//                .tag("Profile")
-        }
-        .environment(viewModel)
-        .onOpenURL(perform: { url in
-            if viewModel.isValidUrlScheme(url) {
-                selectedTab = "Catalog"
-            }
-        })
+        CatalogView()
+            .environment(viewModel)
     }
 }
 
 #Preview {
-    CustomerView(user: User.MOCK_USER, isAnonymous: false)
+    CustomerView()
         .environment(AuthViewModel())
 }
